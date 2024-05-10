4e19626e8312b5b2a16e0351a9384febfb3ab3c4
lilynhuynh <lilynhuynhtx@gmail.com>
2024-05-10 17:59:33 +1000

    Local storage test
    Testing saving div elements into local storage (unsuccessful)

--- public/script.js
+++ public/script.js
@@ -10,20 +10,33 @@ class Messages {
         this.listOfMsgs = []
     }
 
-    addMsg(name, feeling, affirmation, goal, answer, question, id) {
-        var newMsg = new Message(name, feeling, affirmation, goal, answer, question, id)
+    addMsg(name, feeling, affirmation, goal, answer, question, id, date) {
+        var newMsg = new Message(name, feeling, affirmation, goal, answer, question, id, date)
         this.listOfMsgs.push(newMsg)
         this.displayMsg(newMsg)
     }
 
     displayMsg(msg) {
-        let item = document.createElement("li")
-        item.id = "msg-card"
 
-        var msgCard = document.createElement("div")
+        // let item = document.createElement("li")
+        // item.id = "msg-card"
+
+        var msgCard = document.createElement("pre")
         msgCard.id = msg.id
-        msgCard.class = "container"
+        msgCard.className = "container"
 
+        // // Left side of the card
+        // var cardLeft = document.createElement("div")
+        // cardLeft.className = "form-col-left"
+        // msgCard.appendChild(cardLeft)
+
+        // var nameRow = document.createElement("div")
+        // nameRow.class = "form-row"
+        // cardLeft.appendChild(nameRow)
+
+        // var partnerName = document.createElement("p")
+        // partnerName.innerText = `From: ${msg.name}`
+        // nameRow.appendChild(partnerName)
 
         msgCard.innerHTML =
             `
@@ -50,25 +63,34 @@ class Messages {
                     <p>${msg.answer}</p>
             </div>
             `
-        item.append(msgCard)
-        msgList.appendChild(item)
+        // item.append(msgCard)
+        // msgList.appendChild(item)
         form.reset()
 
         let delButton = document.createElement("button")
         let delButtonText = document.createTextNode("Delete")
         delButton.appendChild(delButtonText)
-        item.appendChild(delButton)
+        msgCard.appendChild(delButton)
 
         delButton.addEventListener("click", function (event) {
-            item.remove()
-            listOfMsgs = listOfMsgs.filter(msg => msg.id !== item.getAttribute('msg-card'))
+            listOfMsgs = listOfMsgs.filter(msg => msg.id !== msgCard.getAttribute('msg-card'))
             console.log(listOfMsgs)
         })
+
+        let storeMsg = JSON.parse(localStorage.getItem("storeMsg"))
+        if (storeMsg == null){
+            storeMsg = [msgCard];
+        } else {
+            storeMsg.push(msgCard);
+        }
+        localStorage.setItem("storeMsg", JSON.stringify(msgCard, undefined, 2));
+        updateLog();
     }
+
 }
 
 class Message {
-    constructor(name, feeling, affirmation, goal, answer, question, id) {
+    constructor(name, feeling, affirmation, goal, answer, question, id, date) {
         this.name = name
         this.feeling = feeling
         this.affirmation = affirmation
@@ -76,6 +98,7 @@ class Message {
         this.answer = answer
         this.question = question
         this.id = id
+        this.date = date
     }
 }
 
@@ -92,39 +115,55 @@ form.addEventListener("submit", function (event) {
     console.log(document.getElementById("questionPrompt").innerText)
     var msgQuestion = document.getElementById("questionPrompt").innerText
     var msgId = Math.floor(Math.random() * Date.now())
-    console.log(msgName, msgFeel, msgAffirm, msgGoal, msgAnswer, msgQuestion, msgId)
-    listMsgs.addMsg(msgName, msgFeel, msgAffirm, msgGoal, msgAnswer, msgQuestion, msgId)
+    var msgDate = document.getElementById("today-date").innerText
+    console.log(msgName, msgFeel, msgAffirm, msgGoal, msgAnswer, msgQuestion, msgId, msgDate)
+    listMsgs.addMsg(msgName, msgFeel, msgAffirm, msgGoal, msgAnswer, msgQuestion, msgId, msgDate)
     console.log(listMsgs.listOfMsgs)
 })
 
+// insane question list
 // Would You Rather Questions provided by Paired Magazine
 var questionList =
     {
-        question0:["Would you rather eat pizza or ice cream as the only food for the rest of your life?",
+        question0:[
+            "Would you rather eat pizza or ice cream as the only food for the rest of your life?",
             "Pizza",
             "Ice Cream"
         ],
-        question1:["Would you rather go on a date to a water park or a theme park?",
+        question1:[
+            "Would you rather go on a date to a water park or a theme park?",
             "Water Park",
             "Theme Park"
+        ],
+        question2:[
+            "Would you rather give up your favorite food or your favorite TV show?",
+            "Favorite Food",
+            "Favorite TV Show"
+        ],
+        question3:[
+            "Would you rather find true love or win the lottery?",
+            "True Love",
+            "Lottery"
+        ],
+        question4:[
+            "Would you rather date someone with bad breath or BO?",
+            "Bad Breath",
+            "BO"
         ]
     }
 
-console.log(questionList)
 
 function generateQuestion(){
-    var questionGenerator = String(Math.floor(Math.random(1)));
+    var questionGenerator = String(Math.floor(Math.random(questionList.length)));
     var questionStr = "question" + questionGenerator;
 
     var chosenQuestion = questionList[questionStr]
     if (chosenQuestion == "null"){
         console.log("No more questions left!")
     }
-    console.log(chosenQuestion)
 
     var changePrompt = document.getElementById("questionPrompt");
     changePrompt.innerText = chosenQuestion[0];
-    console.log(changePrompt.innerText)
 
     var changeChoiceAVal = document.getElementById("choiceA");
     changeChoiceAVal.value = chosenQuestion[1];
@@ -139,8 +178,33 @@ function generateQuestion(){
     changeChoiceBTxt.innerHTML = chosenQuestion[2];
 
     delete questionList[questionStr];
-    console.log(questionList)
 }
 window.onload = generateQuestion();
+
+updateLog()
+
+
+let clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', function() {
+    localStorage.removeItem('storeMsg');
+    updateLog();
+})
+
+function updateLog() {
+    let list = msgList;
+    list.innerHTML = "";
+
+    let storeMsg = JSON.parse(localStorage.getItem("storeMsg"))
+    if (storeMsg !== null) {
+        storeMsg.forEach((msg) => {
+            let listItem = document.createElement("li")
+            listItem.textContent = msg
+            list.appendChild(listItem)
+        })
+    } else {
+        console.log("error")
+    }
+}
 
 console.log("executed")
